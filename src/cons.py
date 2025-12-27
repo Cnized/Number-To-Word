cae58568c57d85396c447e4d3149491041533780
Kian Kheiri <kingkk8181@gmail.com>
2025-12-27 13:57:25 +0330

    Changes to code, cons.py added

---
+++ src/cons.py
@@ -0,0 +1,11 @@
+Under_20 = [ "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+            "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+]
+Tens = [ "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" ]
+
+More_than_hundred = {
+    100: "Hundred",
+    1000: "Thousand",
+    1000000: "Million",
+    1000000000: "Billion"
+}
---
+++ src/main.py
@@ -0,0 +1,28 @@
+from src.cons import Under_20, Tens, More_than_hundred
+
+
+def number_to_word(num):
+    if num < 20:
+        return Under_20[num]
+    elif num < 100:
+        remainder = num % 10
+        if remainder == 0:
+            return Tens[num // 10]
+        return Tens[num // 10] + " " + Under_20[remainder]
+    
+    pivot = max([key for key in more_than_hundred if key <= num])
+    if num % pivot == 0:
+        return f'{number_to_word(num // pivot)} {more_than_hundred[pivot]}'
+    return f'{number_to_word(num // pivot)} {more_than_hundred[pivot]} {number_to_word(num % pivot)}'
+
+if __name__ == "__main__":
+    print(number_to_word(1111111))
+    print(number_to_word(2222222))
+    print(number_to_word(3333333))
+    print(number_to_word(4444444))
+    print(number_to_word(5555555))
+    print(number_to_word(6666666))
+    print(number_to_word(7777777))
+    print(number_to_word(8888888))
+    print(number_to_word(9999999))
+    print(number_to_word(1000000))
